a76010e2e940e78536f2529180954485dc0fd45b
moon <moonshadowbusinessgroup@gmail.com>
2021-10-17 20:50:20 +0530

    ok

---
+++ app/src/main/java/com/hadimusthafa/get_coding_in_flow/JsonPlaceHolderApi.java
@@ -0,0 +1,12 @@
+package com.hadimusthafa.get_coding_in_flow;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
